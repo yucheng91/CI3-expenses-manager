28d5c2befc5705cf8b3521384941493583f03639
Ubuntu <ubuntu@ip-172-31-95-44.ec2.internal>
2019-08-15 07:41:21 +0000

    created dropdown list for transaction

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ connection = pymysql.connect(
     database="expenses" #the name of database we want to use
 )
 
+#if PHP doesn't show, perform $ sudo apt-get install --reinstall libapache2-mod-php7.2
+
 app = Flask(__name__)
 
 @app.route('/home')
@@ -27,29 +29,54 @@ def viewall():
      return render_template('transaction_overview.html', data=results)
      
 @app.route('/new-transaction/', methods=["GET"])
-def new_supplier():
-    return render_template('new_transaction.html')
-    
-@app.route('/new-transaction/', methods=["POST"])
 def addtransaction():
-    transaction_name = request.form.get("transaction")
-    category_name = request.form.get("category")
-    mode_name = request.form.get("mode")
-    by_name = request.form.get("by")
-    debit_name = request.form.get("debit")
-    credit_name = request.form.get("credit")
-    cursor = connection.cursor(pymysql.cursors.DictCursor)
-    sql = "INSERT INTO transaction (description) VALUES ('{}')".format(transaction_name);
-    "INSERT INTO categories (name) VALUES ('{}')".format(category_name);
-    "INSERT INTO mode (description) VALUES ('{}')".format(mode_name);
-    "INSERT INTO account (description) VALUES ('{}')".format(by_name);
-    "INSERT INTO transaction (debit) VALUES ('{}')".format(debit_name);
-    "INSERT INTO transaction (credit) VALUES ('{}')".format(credit_name);
-
-    cursor.execute(sql)
-    connection.commit() 
     
-    return "Transaction record has been added"
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    sql = "SELECT * FROM mode"
+    # fetch all genres and store it in a list
+    cursor.execute(sql)
+    mode = []
+    for r in cursor:
+        mode.append({
+            'id' : r['id'],
+            'name': r['mname']
+        })
+        
+    # fetch all media type and store it in a list
+    sql = "SELECT * FROM categories"
+    cursor.execute(sql)
+    categories = []
+    for r in cursor:
+        categories.append({
+            'id':r['id'],
+            'name': r['cname']
+        })
+    
+    # fetch all media type and store it in a list
+    sql = "SELECT * FROM account"
+    cursor.execute(sql)
+    account = []
+    for r in cursor:
+        account.append({
+            'id':r['id'],
+            'name': r['aname']
+        })
+    
+    return render_template('new_transaction.html', mode=mode, categories=categories, account=account)
+    
+# @app.route('/new-transaction/', methods=["POST"])
+# def process_addtransaction():
+#     transaction_name = request.form.get("transaction")
+#     category_name = request.form.get("category")
+#     mode_name = request.form.get("mode")
+#     by_name = request.form.get("by")
+#     debit_name = request.form.get("debit")
+#     credit_name = request.form.get("credit")
+#     cursor = connection.cursor(pymysql.cursors.DictCursor)
+
+#     connection.commit() 
+    
+#     return "Transaction record has been added"
 
 # "magic code" -- boilerplate
 if __name__ == '__main__':
